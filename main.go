7c0b425b65cf5997a7ccb0aafd682ce763106a11
RadesGhani <rades208@gmail.com>
2019-11-12 09:35:16 +0700

    2019/11/12

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"fmt"
+)
+
+type introspectIn struct {
+	Token string 'form: "token"'
+}
+
+func Bootstrap() {
+	log.Println("Ping module bootstrap.")
+}
+
+func Router(r *gin.Engine) {
+	r.POST("/introspect", PostIntrospectHandler)
+}
+
+func PostIntrospectHandler(c *gin.Context) {
+	var input introspectIn
+	c.Bind(&input)
+	fmt.Println(input.Token)
+	c.JSON(200, gin.H{"token": input.Token})
+}
